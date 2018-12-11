89b2936e8d110861550f773f55fdc8156b74d766
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-12-11 11:11:00 -0800

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        //trackingId: "ADD YOUR TRACKING ID HERE",
+        trackingId: 'UA-78685008-2',
       },
     },
     'gatsby-plugin-react-helmet',
